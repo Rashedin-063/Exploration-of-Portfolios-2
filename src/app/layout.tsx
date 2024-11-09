4066c75c7d7f22f9cdf277f28dfda5cbe654e4e2
RU063 <rashedinislam.06@gmail.com>
2024-11-09 18:47:42 +0600

    dark theme applied using shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Inter} from "next/font/google"
 import "./globals.css";
+import { ThemeProvider } from "@/providers/provider";
 
 const inter = Inter({subsets: ["latin"]})
 
@@ -15,11 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className}  antialiased`}
-      >
-        {children}
+    <html lang='en'>
+      <body className={`${inter.className}  antialiased`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
