70843bcc8145d9332b0bdb3c7d0126762d6b92cc
RU063 <rashedinislam.06@gmail.com>
2024-11-09 19:56:51 +0600

    hero section text complete with text-generate-effect from aceternity ui

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "./ui/spotlite"
+import { TextGenerateEffect } from '@/components/ui/text-generate-effect';
 
 const Hero = () => {
   return (
@@ -20,15 +21,22 @@ const Hero = () => {
       {/* background */}
       <div className='h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.3] flex items-center justify-center absolute top-0 left-0'>
         {/* Radial gradient for the container to give a faded look */}
-        <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'> 
-
-        </div>
+        <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
       </div>
-      
+
       {/* hero content */}
       <div className='flex justify-center relative my-20 z-10'>
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h1 className="uppercase text-xs text-center text-blue-100  max-w-80">Dynamic Web Magic with Next.js</h1>
+        <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+          <h2 className='uppercase text-xs text-center text-blue-100  max-w-80'>
+            Dynamic Web Magic with Next.js
+          </h2>
+
+          <TextGenerateEffect
+            words='Transforming Concepts into Seamless Experiences'
+            className='text-center text-[40px] md:text-5xl lg:text-6xl'
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I&apos;m Rashedin Islam, a Next.js Developer based in Bangladesh</p>
         </div>
       </div>
     </div>
