05a4f27fef72f317f584bea6fb07bf94ad718c1c
RU063 <rashedinislam.06@gmail.com>
2024-11-10 11:42:51 +0600

    checking

---
+++ lib/index.ts
@@ -0,0 +1,25 @@
+// is tabnine working?
+// how can I tell?
+
+type Person = {
+  name: string;
+  age: number;
+}
+
+const newPerson: Person = {
+  name: 'John Doe',
+  age: 30,
+}
+
+const newPerson2: Person = {
+  name: 'Jane Doe',
+  age: 28,
+}
+
+function getOlderPerson(person1: Person, person2: Person): Person { 
+  if (person1.age > person2.age) {
+    return person1;
+  } else {
+    return person2;
+  }
+}
